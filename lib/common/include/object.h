572f30dcc2bd6c82b07d3df42445d26d34086961
Benjamin Kellogg <benk691@gmail.com>
2012-05-15 14:40:35 -0700

    Object source code.

--- lib/common/include/object.h
+++ lib/common/include/object.h
@@ -4,9 +4,28 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include "mesh.h"
 
 class Object
 {
+    public:
+        static unsigned int objId = 0;
+        double x;
+        double y;
+        double z;
+        double pitch;
+        double roll;
+        double yaw;
+        const unsigned int id;
+
+    public:
+        Object();
+
+        virtual ~Object() = 0;
+
+        virtual void draw() = 0;
+
+        virtual void update() = 0;
 };
 
 #endif //__OBJECT_H__
---
+++ lib/common/src/object.cpp
@@ -0,0 +1,6 @@
+#include "../include/object.h"
+
+Object::Object()
+    :x(0.0), y(0.0), z(0.0), pitch(0.0), roll(0.0), yaw(0.0), id()
+{}
+
