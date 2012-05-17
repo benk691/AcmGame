4dc90616b6795212e8827ac9dcabeb5706389040
Benjamin Kellogg <benk691@gmail.com>
2012-05-17 07:12:25 -0700

    Adding zmq, an external package for sockets

---
+++ lib/common/include/socket.h
@@ -0,0 +1,10 @@
+#ifndef __SOCKET_H__
+#define __SOCKET_H__
+
+#include "socketexception.h"
+
+class Socket
+{
+};
+
+#endif //__SOCKET_H__
--- lib/common/src/socket.cpp
+++ lib/common/src/socket.cpp
@@ -1,4 +1,6 @@
-
+#include <boost/tokenizer.hpp>
+#include <boost/utility.hpp>
+#include <zmq.h>
 int main()
 {
     return 0;
